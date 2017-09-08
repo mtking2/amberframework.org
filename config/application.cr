fed51c3198c83be2076b41ee8fde0ceb6bf581e1
Elias Perez <eliasjpr@gmail.com>
2017-09-08 11:31:10 -0500

    fix port

--- config/application.cr
+++ config/application.cr
@@ -2,6 +2,10 @@ Amber::Server.instance.config do |app|
   # Server options
   app_path = __FILE__ # Do not change unless you understand what you are doing.
   app.name = "Ambercr.io web application."
+  app.host = "0.0.0.0"
+  opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
+    app.port = opt.to_i
+  end
   app.env = ENV.fetch("AMBER_ENV", "development").colorize(:green).to_s
   app.log = ::Logger.new(STDOUT)
   app.log.level = ::Logger::INFO
--- src/ambercr.io.cr
+++ src/ambercr.io.cr
@@ -6,10 +6,4 @@ require "./**"
 require "./*"
 require "../config/*"
 
-OptionParser.parse! do |opts|
-  opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
-    Amber::Server.settings.port = opt.to_i
-  end
-end
-
 Amber::Server.start
