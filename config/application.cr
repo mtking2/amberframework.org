67494799b6f50fa1c2b091432514ddd8176f4559
Elias Perez <eliasjpr@gmail.com>
2017-09-08 15:08:15 -0500

    Add port binding

--- config/application.cr
+++ config/application.cr
@@ -1,10 +1,12 @@
-Amber::Server.instance.config do |app|
+Amber::Server.configure do |app|
   # Server options
   app_path = __FILE__ # Do not change unless you understand what you are doing.
   app.name = "Ambercr.io web application."
   app.host = "0.0.0.0"
-  opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
-    app.port = opt.to_i
+  OptionParser.parse! do |opts|
+    opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
+      app.port = opt.to_i
+    end
   end
   app.env = ENV.fetch("AMBER_ENV", "development").colorize(:green).to_s
   app.log = ::Logger.new(STDOUT)
